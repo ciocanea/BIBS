1b8a0a589baf0077342695a1b1e561e101a55be6
Calin Ciocanea <c.ciocanea@student.tudelft.nl>
2025-04-12 00:03:23 +0200

    Add User Profile Model

---
+++ bibs/lib/data/models/user_profile_model.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_profile_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_UserProfile _$UserProfileFromJson(Map<String, dynamic> json) => _UserProfile(
+  id: json['id'] as String,
+  username: json['username'] as String,
+);
+
+Map<String, dynamic> _$UserProfileToJson(_UserProfile instance) =>
+    <String, dynamic>{'id': instance.id, 'username': instance.username};
