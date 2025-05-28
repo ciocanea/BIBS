b442e2b8e7496c1ae62ee0355ebdd57f09e005ae
Calin Ciocanea <c.ciocanea@student.tudelft.nl>
2025-05-28 19:36:46 +0200

    Improve profile screen UI

--- bibs/lib/ui/profile/view_models/profile_viewmodel.dart
+++ bibs/lib/ui/profile/view_models/profile_viewmodel.dart
@@ -26,6 +26,29 @@ class ProfileViewmodel extends ChangeNotifier{
   File? _imageFile;
   File? get imageFile => _imageFile;
 
+  String? passwordValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter some text';
+    }
+
+    final password = value;
+
+    final lengthCheck = password.length >= 8;
+    // final upperCheck = RegExp(r'[A-Z]').hasMatch(password);
+    // final numberCheck = RegExp(r'[0-9]').hasMatch(password);
+    // final specialCheck = RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(password);
+    final spaceCheck = !password.contains(' ');
+
+    if (!lengthCheck) return 'Password must be at least 8 characters long.';
+    // if (!upperCheck) return 'Password must contain at least one uppercase letter.';
+    // if (!numberCheck) return 'Password must contain at least one number.';
+    // if (!specialCheck) return 'Password must contain at least one special character.';
+    if (!spaceCheck) return 'Password must not contain any spaces.';
+
+    return null;
+  }
+
+
   Future<Result<void>> load() async {
     try {
       final userProfileResult = await _userRepository.getProfile();
